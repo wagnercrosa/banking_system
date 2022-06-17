9d723a6feb47430a33055e6678d51087048138a8
Wagner Corrêa <wagnerspg@hotmail.com>
2022-06-17 19:29:42 -0300

    Implementação da conta corrente

--- source/Bank/AccountSaving.php
+++ source/Bank/AccountSaving.php
@@ -7,30 +7,44 @@ use Source\App\Message;
 
 class AccountSaving extends Account
 {
+    /**
+     * @var float
+     */
     private $interest;
 
+    /**
+     * @param $branch
+     * @param $account
+     * @param Client $client
+     * @param $balance
+     */
     public function __construct($branch, $account, Client $client, $balance)
     {
         parent::__construct($branch, $account, $client, $balance);
         $this->interest = 0.006;
     }
 
-    public function deposit($value)
+    /**
+     * @param $value
+     * @return void
+     */
+    public function deposit($value): void
     {
-        // TODO: Implement deposit() method.
         $this->balance = $value + ($value * $this->interest);
-        Message::show("Depósito de {$this->toBrl($this->balance)} realizado com sucesso!", Message::ACCEPT);
+        Message::show("Depósito de <strong>{$this->toBrl($this->balance)}</strong> realizado com sucesso!", Message::ACCEPT);
     }
 
-    public function withdrawal($value)
+    /**
+     * @param $value
+     * @return void
+     */
+    public function withdrawal($value): void
     {
-        // TODO: Implement withdrawal() method.
-
         if($this->balance >= $value) {
             $this->balance -= abs($value);
-            Message::show("Saque de {$this->toBrl($value)} realizado com sucesso!", Message::ERROR);
+            Message::show("Saque de <strong>{$this->toBrl($value)}</<strong> realizado com sucesso!", Message::ERROR);
         }else{
-            Message::show("Saldo insuficientel, você tem {$this->toBrl($this->balance)}!", Message::WARNING);
+            Message::show("Saldo insuficientel, você tem <strong>{$this->toBrl($this->balance)}</strong>!", Message::WARNING);
         }
     }
 
