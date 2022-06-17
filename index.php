efa566cb8db3258e59044082711055e340a4caf9
Wagner Corrêa <wagnerspg@hotmail.com>
2022-06-17 19:28:49 -0300

    Testando a conta corrente.

--- index.php
+++ index.php
@@ -4,19 +4,17 @@ $title = "Banking System";
 require __DIR__ . "/source/autoload.php";
 require __DIR__ . "/public/template.php";
 
-$account = new \Source\Bank\AccountSaving(
+$account = new \Source\Bank\AccountCurrent(
   "1865",
   "14529-1",
   new \Source\App\Client("Wagner", "Corrêa"),
-  "0"
+  "1000",
+  1000
 );
 
 var_dump($account);
 
-$account->deposit(500);
-$account->withdrawal(499);
-$account->withdrawal(4);
-$account->withdrawal(1000);
+$account->withdrawal(4000);
 $account->extract();
 
 
